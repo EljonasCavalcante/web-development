6116776d8c537d533db5fed586ad065b38748720
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-28 14:47:07 -0300

    Feat:creating dados 2 js

---
+++ dados_2.js
@@ -0,0 +1,28 @@
+
+
+// dois jeito de fazer a mesma coisa, referente a desconto.
+console.log(19.9 * 0.6);// valor literal
+
+
+// esse abaixo fica mais fácil entender o funcionamento
+let price = 19.90;
+let discount = 0.4;
+let priceDiscount = price * (1 - discount);
+console.log(priceDiscount);
+
+let name_1 = "book";
+let category = "stationery";
+console.log("Produc: " + name_1 
+ + ", Category: " + category 
+ + " , Price: " + price 
+ + " , Discount: " + discount
+ );
+
+ 
+
+//preço= price
+//desconto=discount
+// livro = book
+// categoria = category
+// papelaria = stationery
+// produto = product
