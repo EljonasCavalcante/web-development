f6f7f2bf48fd0fa41d1c0e4a76fa85c13e1331cd
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-27 22:45:55 -0300

    Feat: creating let or var

---
+++ dados.js
@@ -0,0 +1,14 @@
+let name_1 = "caneta"
+let quantity = 10;
+let price = 6.4;
+let impost = 6.4;
+let finalPrice = price + impost;
+
+console.log(name_1);
+console.log(quantity);
+console.log(price);
+console.log(impost);
+console.log(finalPrice);
+
+name_1 = "caneta BIC"
+console.log(name_1)
