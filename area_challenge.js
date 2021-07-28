926fb9abc2e810de6e376469771b8db7dcfc1e43
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-28 16:53:47 -0300

    Feat:name change and change challenge

--- area_challenge.js
+++ area_challenge.js
@@ -6,8 +6,8 @@ Quero saber o raio de 10 em metros quadrados
 */
 
 const PI = 3.141592;
-let raio = 10;
-let areaCirc = PI * raio * raio;
+let radius = 10;
+let areaCirc = PI * radius * radius;
 
 console.log ("Area da Circufência: "+ areaCirc + " m²")
 
@@ -16,6 +16,6 @@ console.log ("Area da Circufência: "+ areaCirc + " m²")
 /*Math é um objeto embutido que tem propriedades e métodos para constantes e funções matemáticas. Não é um objeto de função.*/
 
 // outro jeeto de resolver o desafio.
-areaCirc = Math.PI * raio * raio; // O Math mostrou o PI exato.
+areaCirc = Math.PI * radius * radius; // O Math mostrou o PI exato.
 console.log ("Area da Circufência: "+ areaCirc + " m²")
 
---
+++ chanllenge_swap.js
@@ -0,0 +1,10 @@
+let a = 7;
+let b = 94;
+
+// o desafio é troca ... no caso seria a = 94 e b = 7
+ let temp = a; // essa ainda é 7 que recebeu o nome de "temp"
+ a = b;
+ b = temp;
+
+ console.log("a = " + a);
+ console.log("b = " + b);
