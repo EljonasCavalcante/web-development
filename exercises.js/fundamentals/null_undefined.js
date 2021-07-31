080133f0bb50cf3aee91ddf19448fc5b82e4a87a
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-31 13:29:06 -0300

    Feat: creating functon and null js

---
+++ exercises.js/fundamentals/null_undefined.js
@@ -0,0 +1,8 @@
+let valor // não inializada
+console.log(valor)
+
+valor = null // ausência de valor
+console.log(valor)
+//console.log(valor.toString()) //erro
+ const produto = {}
+ console.log(produto.price)
