d2b936b6ffff06e0bab925b79631170155bea61c
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-07-31 14:33:54 -0300

    Feat: creating hoisting,let,obejct and name js

---
+++ exercises.js/fundamentals/obejct2.js
@@ -0,0 +1,10 @@
+console.log(typeof Object)
+console.log(typeof new Object)
+
+const cliente = function () {}
+console.log(typeof cliente)
+console.log(typeof new cliente)
+
+class produto {}
+console.log(typeof produto)
+console.log(typeof new produto ())
---
+++ exercises.js/fundamentals/parNomeValor.js
@@ -0,0 +1,20 @@
+// par nome/valor
+
+const saudacao = 'Opa'
+function exec() {
+  const saudacao = 'fala'
+  return saudacao
+}
+
+const cliente ={
+  nome: 'perdro',
+  idade:32,
+  peso: 90,
+  endereço:{
+      logradouro:'rua muito show',
+      numero:123
+  }
+}
+
+console.log(saudacao)
+console.log(exec())
