8786dd8050cb4c42b3d410bf70bf749f99df6db1
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-08-01 16:47:56 -0300

    Feat:creating destructuring two js

---
+++ Destructuring3.js
@@ -0,0 +1,9 @@
+function rand({ min = 0, max = 1000 }) {
+  const valor = Math.random() * (max - min) + min //Este exemplo retorna um número entre dois valores definidos. O valor retornado será maior ou igual a min, e menor que max.
+  return Math.floor(valor)//A função Math.floor(x) retorna o menor número inteiro dentre o número "x(um numero)".
+}
+
+const valor = { max:50, min:40} // {obj} poderia subtituir nome valor por Obj.
+console.log(rand(valor)) 
+  
+
