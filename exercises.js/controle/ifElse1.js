027dd720fb0cef8ca8e4701c4981557639ded9c3
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-08-07 10:39:29 -0300

    Feat: creating If and Else js

---
+++ exercises.js/controle/ifElse1.js
@@ -0,0 +1,10 @@
+const imprimirRsultado = function(nota) {
+  if(nota >= 7) {
+    console.log('Aprovado')
+  } else {
+    console.log('Reprovado')
+  }
+}
+
+imprimirRsultado(10)
+imprimirRsultado(5)
