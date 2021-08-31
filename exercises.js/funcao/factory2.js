3f531107d578df954d4d0195a9fd1c51c631729d
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-08-31 20:51:17 -0300

    Feat: Creating factory2  JS

---
+++ exercises.js/funcao/factory2.js
@@ -0,0 +1,11 @@
+// Essa função criar produtos apartir do consolo.log , observe abaixo.
+function criarProduto(nome, preco) {
+  return{
+    nome,
+    preco,
+    desconto: 0.1
+  }
+}
+
+console.log(criarProduto('Notebook', 2199.49))
+console.log(criarProduto('iPad', 1199.49))
