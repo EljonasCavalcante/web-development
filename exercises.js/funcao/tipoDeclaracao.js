6b81721153235b9b08b16271caa2199af338d70c
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-08-31 20:15:49 -0300

    Feat: types of functions JS

---
+++ exercises.js/funcao/tipoDeclaracao.js
@@ -0,0 +1,15 @@
+/* function declation ( essa função tem vantagem de se chamdada antes sem dá erro*/
+function soma (x, y) {
+  return x +y
+}
+
+// function expression
+const sub = function (x, y) { 
+  return x - y  
+}
+
+// named function expresssion
+
+const mult = function mult(x, y) {
+  return x * y
+}
