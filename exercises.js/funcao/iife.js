727c1d9114cfe8e42fd03ac53e7bb3ab3b3f86f8
EljonasCavalcante <eljonascavalcante@gmail.com>
2021-09-04 17:14:21 -0300

    Feat: creting iife JS

---
+++ exercises.js/funcao/iife.js
@@ -0,0 +1,8 @@
+// IIFE -> immediately Invoked Function Expression
+
+(function() {
+  console.log('será executado na hora')
+  console.log('Foge do escopo mais abrangente')
+}) ()
+
+// serve para fugir do escopo global
