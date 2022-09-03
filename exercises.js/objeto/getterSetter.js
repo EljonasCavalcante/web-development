b214dc25625e11c5a40087931980c086cfd1e506
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-02 22:47:31 -0300

    fix : code update

--- exercises.js/objeto/getterSetter.js
+++ exercises.js/objeto/getterSetter.js
@@ -1,11 +1,15 @@
-const sequecia = {
-    _valor : 1, // convenção
-    get valor() { return this._valor++},
-    set valor(valor) {
-        if(valor > this._valor){
+const sequencia = {
+    _valor: 1, // convenção
+    get valor() { return this._valor++ },
+    set valor(valor) { 
+        if(valor > this._valor) {
             this._valor = valor
         }
     }
 }
 
-console.log(sequecia.valor,sequecia.valor,sequecia.valor) // 1 2 3
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 1000
+console.log(sequencia.valor, sequencia.valor)
+sequencia.valor = 900
+console.log(sequencia.valor, sequencia.valor)
