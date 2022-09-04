310ffb20c9ac01eb34b5482cf6050f5ef36ba563
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-03 22:43:52 -0300

    callback function

--- exercises.js/funcao/callApply.js
+++ exercises.js/funcao/callApply.js
@@ -1,12 +1,12 @@
 function getPreco(imposto = 0, moeda = 'R$') {
-  return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
+    return `${moeda} ${this.preco * (1 - this.desc) * (1 + imposto)}`
 }
 
 const produto = {
-  nome: 'Notebook',
-  preco: 4589,
-  desc: 0.15,
-  getPreco
+    nome: 'Notebook',
+    preco: 4589,
+    desc: 0.15,
+    getPreco
 }
 
 global.preco = 20
@@ -14,15 +14,10 @@ global.desc = 0.1
 console.log(getPreco())
 console.log(produto.getPreco())
 
-
-// outra  exemplo abaixo
-
-const carro = { preco: 49990, desc: 0.20}
+const carro = { preco: 49990, desc: 0.20 }
 
 console.log(getPreco.call(carro))
 console.log(getPreco.apply(carro))
 
-
 console.log(getPreco.call(carro, 0.17, '$'))
 console.log(getPreco.apply(global, [0.17, '$']))
-
