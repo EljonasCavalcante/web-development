4fd292091bbd1276701b1c7438bda86869a8f0cf
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-03 22:43:34 -0300

    arrow two

--- exercises.js/funcao/arrowFunction2.js
+++ exercises.js/funcao/arrowFunction2.js
@@ -1,10 +1,10 @@
 function Pessoa() {
-  this.idade = 0
+    this.idade = 0
 
-  setInterval(() => {
-    this.idade++
-    console.log(this.idade)
-  }, 1000)
+    setInterval(() => {
+        this.idade++
+        console.log(this.idade)
+    }, 1000)
 }
 
-new Pessoa  
+new Pessoa
--- exercises.js/funcao/arrowFunction3.js
+++ exercises.js/funcao/arrowFunction3.js
@@ -1,18 +1,18 @@
-let comparaConThis = function (param) {
-  console.log(this === param)  // estritamente igual
+let comparaComThis = function (param) {
+    console.log(this === param)
 }
 
-comparaConThis(global)
+comparaComThis(global)
 
 const obj = {}
-comparaConThis = comparaConThis.bind(obj)
-comparaConThis(global)
-comparaConThis(obj)
+comparaComThis = comparaComThis.bind(obj)
+comparaComThis(global)
+comparaComThis(obj)
 
-let comparaConThisArrow = param => console.log(this === param)
-comparaConThisArrow(global)
-comparaConThisArrow(module.exports)
+let comparaComThisArrow = param => console.log(this === param)
+comparaComThisArrow(global)
+comparaComThisArrow(module.exports)
 
-comparaConThisArrow = comparaConThisArrow.bind(obj)
-comparaConThisArrow(obj)
-comparaConThisArrow(module.exports)
+comparaComThisArrow = comparaComThisArrow.bind(obj)
+comparaComThisArrow(obj)
+comparaComThisArrow(module.exports)
