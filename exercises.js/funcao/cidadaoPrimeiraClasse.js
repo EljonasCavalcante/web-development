49ac6afcd0a506b48d8701bb43fff78fdd1d5382
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-03 22:47:24 -0300

    array function

--- exercises.js/funcao/cidadaoPrimeiraClasse.js
+++ exercises.js/funcao/cidadaoPrimeiraClasse.js
@@ -1,37 +1,35 @@
-// Função em JS é first-class 
-// higher-order function
+// Função em JS é First-Class Object (Citizens)
+// Higher-order function
 
 // criar de forma literal
+function fun1() { }
 
-function fun1(){}
+// Armazenar em uma variável
+const fun2 = function () { }
 
-// armagezenar em um variável
-const fun2 = function () {}
-
-// Armazenar em um Array
-const array = [function (a, b) {return a + b}, fun1, fun2]
+// Armazenar em um array
+const array = [function (a, b) { return a + b }, fun1, fun2]
 console.log(array[0](2, 3))
- 
-// Arnagenar en um atributo de objeto
+
+// Armazenar em um atributo de objeto
 const obj = {}
-obj.falar = function () { return 'Opa'}
+obj.falar = function () { return 'Opa' }
 console.log(obj.falar())
 
 // Passar função como param
-
 function run(fun) {
-  fun()
+    fun()
 }
 
-run(function (){
-  console.log('executando')
-})
+run(function () { console.log('Executando...') })
 
-// função pode retornar / conter função
-
+// Um função pode retornar/conter um função
 function soma(a, b) {
-  return function (c) {
-    console.log(a + b + c)
-  }
+    return function (c) {
+        console.log(a + b + c)
+    }
 }
- soma(2, 3)(4)
+
+soma(2, 3)(4)
+const cincoMais = soma(2, 3)
+cincoMais(4)
