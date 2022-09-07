91de8316e88ccf98c6caa85453bbb2039458769f
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-06 23:51:21 -0300

    this one

--- exercises.js/funcao/thisEBind1.js
+++ exercises.js/funcao/thisEBind1.js
@@ -1,14 +1,13 @@
 const pessoa = {
     saudacao: 'Bom dia!',
     falar() {
-      console.log(this.saudacao)
+        console.log(this.saudacao)
     }
 }
 
-pessoa.falar() 
+pessoa.falar()
 const falar = pessoa.falar
-falar()
+falar() // conflito entre paradigmas: funcional e OO
 
 const falarDePessoa = pessoa.falar.bind(pessoa)
 falarDePessoa()
-
