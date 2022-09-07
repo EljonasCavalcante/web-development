90f72d9c10d273ce80d7881a2c735222c440bc15
EljonasCavalcante <eljonascavalcante@gmail.com>
2022-09-06 23:51:31 -0300

    this two

--- exercises.js/funcao/thisEBind2.js
+++ exercises.js/funcao/thisEBind2.js
@@ -1,9 +1,11 @@
- function Pessoa() {
-   this.idade = 0
-   const self = this
-   setInterval(function() {
-     self.idade++
-     console.log(self.idade)
-   }, 1000)
- }
- new Pessoa
+function Pessoa() {
+    this.idade = 0
+
+    const self = this
+    setInterval(function() {
+        self.idade++
+        console.log(self.idade)
+    }/*.bind(this)*/, 1000)
+}
+
+new Pessoa
